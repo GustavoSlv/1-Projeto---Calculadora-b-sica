5ef328cd62f03534f8d2466aee0bcdf476bd9b85
Gustavo Silva <gustavo9257silva@gmail.com>
2022-04-02 11:41:43 -0300

    mini-projeto finalizado 02/04

--- soma.js
+++ soma.js
@@ -1,52 +1,54 @@
-/*Função dos botões */ /*FALTA FAZER OS BOTÕES FUNCIONAR*/
- 
- function somaDosValores(num1,num2){
-    num1 = prompt('Digite o primeiro valor: ');
-    num1 = parseInt(num1);
-    num2 = prompt('Digite o segundo valor: ')
-    num2 = parseInt(num2);
 
-    resultado = num1 + num2;
+function cliqueSoma(valor1, valor2) {
+    //pega os valores do campo de texto e converte para n° decimal e armazena nas variaveis
+    valor1 = parseFloat(document.getElementById('txtA').value);
+    valor2 = parseFloat(document.getElementById('txtB').value);
 
-    return alert(`A soma dos valores é ${resultado}`);
+    //verifica se os campos de texto estão vazios, caso esteja irá aparecer um alert
+    if (document.getElementById('txtA').value == '' || document.getElementById('txtB').value == '') {
+        alert('Preencha os campos corretamente')
+    } else {
+        //paga os valores das variaveis, faz a soma e aprensenta na tela na tag p id resultado
+        document.getElementById('resultado').innerHTML = (valor1 + valor2).toFixed(1);
+    }
 }
 
-function subtracaoDosValores(num1,num2){
-    num1 = prompt('Digite o primeiro valor: ');
-    num1 = parseInt(num1);
-    num2 = prompt('Digite o segundo valor: ')
-    num2 = parseInt(num2);
+function cliqueSubtracao(valor1, valor2) {
 
-    resultado = num1 - num2;
 
-    return alert(`A subtração dos valores é ${resultado}`);
+    valor1 = parseFloat(document.getElementById('txtA').value);
+    valor2 = parseFloat(document.getElementById('txtB').value);
+
+    if (document.getElementById('txtA').value == '' || document.getElementById('txtB').value == '') {
+        alert('Preencha os campos corretamente')
+    } else {
+        //paga os valores das variaveis, faz a subtração e aprensenta na tela na tag p id resultado
+        document.getElementById('resultado').innerHTML = (valor1 - valor2).toFixed(1);
+    }
 }
 
+function cliqueDivisao(valor1, valor2) {
 
-function multiDosValores(num1,num2){
-    num1 = prompt('Digite o primeiro valor: ');
-    num1 = parseInt(num1);
-    num2 = prompt('Digite o segundo valor: ')
-    num2 = parseInt(num2);
 
-    resultado = num1 * num2;
+    valor1 = parseFloat(document.getElementById('txtA').value);
+    valor2 = parseFloat(document.getElementById('txtB').value);
 
-    return alert(`A multiplicação dos valores é ${resultado}`);
+    if (document.getElementById('txtA').value == '' || document.getElementById('txtB').value == '') {
+        alert('Preencha os campos corretamente')
+    } else {
+        document.getElementById('resultado').innerHTML = (valor1 / valor2).toFixed(1);
+    }
 }
 
+function cliqueMultiplicacao(valor1, valor2) {
 
-function divisaoDosValores(num1,num2){
-    num1 = prompt('Digite o primeiro valor: ');
-    num1 = parseInt(num1);
-    num2 = prompt('Digite o segundo valor: ')
-    num2 = parseInt(num2);
 
-    resultado = num1 / num2;
+    valor1 = parseFloat(document.getElementById('txtA').value);
+    valor2 = parseFloat(document.getElementById('txtB').value);
 
-    return alert(`A divisao dos valores é ${resultado}`);
+    if (document.getElementById('txtA').value == '' || document.getElementById('txtB').value == '') {
+        alert('Preencha os campos corretamente')
+    } else {
+        document.getElementById('resultado').innerHTML = (valor1 * valor2).toFixed(1);
+    }
 }
-
-somaDosValores()
-subtracaoDosValores()
-multiDosValores()
-divisaoDosValores()
